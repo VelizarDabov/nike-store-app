5bbdd3e0a42e7945de5051f50c959e0364a73a74
Velizar Dabov <velizardabov@gmail.com>
2024-03-25 14:19:35 +0200

    startup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => (
+  <main className="relative">
+    <section className="xl:padding-1 wide padding-r padding-b">Hero</section>
+    <section className="padding">Popular products</section>
+    <section className="padding">Super quality</section>
+    <section className="padding py-10">Services</section>
+    <section className="padding">SpecialOffers</section>
+    <section className="bg-pale-blue padding">CustomerReviews</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+  </main>
+);
+export default App;
